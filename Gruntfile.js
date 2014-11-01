12735a633855856803e950e4aea4e6e65c402063
francesco-strazzullo <me@francescostrazzullo.info>
2014-11-01 15:16:27 +0100

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          keepalive: true,
+          base:'app'
+        }
+      }
+    },
+    'ftp-deploy': {
+        build: {
+          auth: {
+            host: 'ftp.wequote.it',
+            port: 21,
+            authKey: 'hkt'
+          },
+          src: 'app/',
+          dest: '/public_html',
+          exclusions: ['.htaccess']
+        }
+      }
+  });
+
+    
+  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+   
+    grunt.task.run([
+      'connect'
+    ]);
+  });
+
+  grunt.registerTask('deploy', [
+    'ftp-deploy'
+  ]);
+};
